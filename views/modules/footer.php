cef4de79fdada0b665b49015e26862de89251f53
Wilson Lisboa <wilsonlisboa0@gmail.com>
2020-10-27 11:55:18 -0300

    cargando estilos y script para las tablas

--- views/modules/footer.php
+++ views/modules/footer.php
@@ -1,5 +1,9 @@
 <footer class="main-footer footer-fuente">
     <!-- To the right -->
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.0.0
+    </div>
+    <!-- Titulo a la izquierda -->
     <div class="float-right d-none d-sm-inline">
       Sunrise Company
     </div>
