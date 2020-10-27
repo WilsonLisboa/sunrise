13b3dd261c12ea3d8036748df67eb8e6c719d93a
Wilson Lisboa <wilsonlisboa0@gmail.com>
2020-10-27 13:06:59 -0300

    incorporacion de estilos y script para tablas

--- views/modules/head.php
+++ views/modules/head.php
@@ -5,19 +5,25 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>Sunrise | Inicio</title>
+  
   <!-- Hoja de estilo propia -->
   <link rel="stylesheet" href="<?php echo SERVERURL;?>/styles/style.css">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="<?php echo SERVERURL;?>/styles/all.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo SERVERURL;?>/styles/adminlte.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?php echo SERVERURL;?>/styles/icheck-bootstrap.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo SERVERURL;?>/styles/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="<?php echo SERVERURL;?>/styles/responsive.bootstrap4.css">
+
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <!-- Google font -->
   <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300&display=swap" rel="stylesheet">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="../styles/icheck-bootstrap.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
 </head>
--- views/modules/script.php
+++ views/modules/script.php
@@ -1,9 +1,18 @@
 <!-- jQuery -->
 <script src="<?php echo SERVERURL;?>/js/jquery.js"></script>
 <!-- Bootstrap 4 -->
-<script src="<?php echo SERVERURL;?>/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo SERVERURL;?>/js/bootstrap.bundle.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo SERVERURL;?>/js/adminlte.js"></script>
+<!-- DataTables -->
+<script src="<?php echo SERVERURL;?>/js/jquery.dataTables.js"></script>
+<script src="<?php echo SERVERURL;?>/js/dataTables.bootstrap4.js"></script>
+<script src="<?php echo SERVERURL;?>/js/dataTables.responsive.js"></script>
+<script src="<?php echo SERVERURL;?>/js/responsive.bootstrap4.js"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo SERVERURL;?>dist/js/demo.js"></script>
+
 <!-- Iconos de ionicons 5.1.2.-->
 <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 <!-- Iconos de ionicons 5.2.3. -->
